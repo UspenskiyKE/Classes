23dc04bbdd59e082e04bf3c88e22a72dc4be82fc
Kirill <uspenskiyke@mail.ru>
2022-02-21 20:36:59 +0400

    Первый коммит

---
+++ src/com/company/Book.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Book {
+    private String bookCaption;
+    private Author bookAuthor;
+    private int bookPublishingYear;
+
+    public Book(String bookCaption, Author bookAuthor, int bookPublishingYear) {
+        this.bookCaption=bookCaption;
+        this.bookAuthor=bookAuthor;
+        this.bookPublishingYear=bookPublishingYear;
+    }
+
+    public String getBookCaption() {
+        return bookCaption;
+    }
+    public Author getBookAuthor() {
+        return bookAuthor;
+    }
+    public int getBookPublishingYear() {
+        return bookPublishingYear;
+    }
+    public void setBookPublishingYear(int bookPublishingYear) {
+        this.bookPublishingYear=bookPublishingYear;
+    }
+
+}
