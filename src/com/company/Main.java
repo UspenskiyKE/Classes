d541c6c59da6ceead1128c0bc7a13e64f2664bdc
Kirill <uspenskiyke@mail.ru>
2022-02-21 20:41:37 +0400

    Домашнее задание по теме "Классы"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Author firstAuthor=new Author("Лев","Толстой");
+        Author secondAuthor=new Author("Стивен","Кинг");
+
+        Book firstBook=new Book("Война и мир",firstAuthor,2008);
+        Book secondBook=new Book("Оно",secondAuthor,2015);
+
+        System.out.println("firstBook.getBookCaption() = " + firstBook.getBookCaption());
+        System.out.println("firstBook.getBookAuthor() = " + firstBook.getBookAuthor().getFirstName()+" "+firstBook.getBookAuthor().getLastName());
+        System.out.println("firstBook.getBookPublishingYear() = " + firstBook.getBookPublishingYear());
+        System.out.println();
+        System.out.println("secondBook.getBookCaption() = " + secondBook.getBookCaption());
+        System.out.println("secondBook.getBookAuthor() = " + secondBook.getBookAuthor().getFirstName()+" "+secondBook.getBookAuthor().getLastName());
+        System.out.println("secondBook.getBookPublishingYear() = " + secondBook.getBookPublishingYear());
+        System.out.println();
+        secondBook.setBookPublishingYear(2020);
+        System.out.println("secondBook.getBookPublishingYear() = " + secondBook.getBookPublishingYear());
 
 
     }
