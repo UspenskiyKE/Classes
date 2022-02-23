c6bdf5ae937a49ca3051f2398a48620c213a357b
Kirill <uspenskiyke@mail.ru>
2022-02-23 21:12:37 +0400

    Изменения с прошлого дз для темы "Методы объектов".

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,16 +1,23 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Book {
+    private final String id;
     private String bookCaption;
     private Author bookAuthor;
     private int bookPublishingYear;
 
-    public Book(String bookCaption, Author bookAuthor, int bookPublishingYear) {
+    public Book(String id, String bookCaption, Author bookAuthor, int bookPublishingYear) {
+        this.id=id;
         this.bookCaption=bookCaption;
         this.bookAuthor=bookAuthor;
         this.bookPublishingYear=bookPublishingYear;
     }
 
+   public String getId() {
+        return id;
+   }
     public String getBookCaption() {
         return bookCaption;
     }
@@ -22,6 +29,22 @@ public class Book {
     }
     public void setBookPublishingYear(int bookPublishingYear) {
         this.bookPublishingYear=bookPublishingYear;
+    }
+    @Override
+    public String toString() {
+        return bookAuthor.toString() + " \"" + bookCaption+"\". Год издания: "+bookPublishingYear;
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c2 = (Book) other;
+        return id.equals(c2.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
  }
